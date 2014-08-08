19f694bc4c5c5d699657c11af31c6e0ab7f9973c
Gitthere <ichi4g@gmail.com>
2014-08-07 22:42:04 -1000

    moved usage code inside of document.ready and definition outside

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-$(document).ready(function() {
+//create class
+function Box(color) {
+  this.color = color;
+  this.height = 50;
+  this.width = 50;
+};
 
-  //create class
-  function Box(color) {
-    this.color = color;
-    this.height = 50;
-    this.width = 50;
-  };
+//draw method
+Box.prototype.draw = function() {
+  var newdiv = $('<div>');
+  $('body').append(newdiv);
+  newdiv.css('background-color', this.color);
+  newdiv.css('height', this.height);
+  newdiv.css('width', this.width);
+  var colorHold = this.color;
+  $(newdiv).click(function(){
+    alert(colorHold + ' has been clicked.');
+  });
+};
+
   //**** can be done without creating 
   //new subclasses with code below
   // //draw blue and red//new instances of box
@@ -14,54 +26,44 @@ $(document).ready(function() {
   // var greendiv = new Box ('#00FF00');
   // reddiv.height = 100;
   // reddiv.width = 100;
-
   //create bluediv subclass to box
-  function  Bluediv(color) {
-    Box.call(this, color);
-  };
-  //properties of box to Bluediv
-  Bluediv.prototype = new Box();
+ 
+function  Bluediv(color) {
+  Box.call(this, color);
+};
+//properties of box to Bluediv
+Bluediv.prototype = new Box();
+
+
+//create Reddiv subclass to box
+function Reddiv(color){
+  Box.call(this, color);
+};
+
+//access to Box prototype chain
+Reddiv.prototype = new Box();
+
+
+function Greendiv(color) {
+  Box.call (this, color);
+};
+
+Greendiv.prototype = new Box();  
+
+
+
+$(document).ready(function() {
+
   //the blue box
   var blueBox = new Bluediv ('#000099');  
 
-
-  //create Reddiv subclass to box
-  function Reddiv(color){
-    Box.call(this, color);
-  };
-
-  //access to Box prototype chain
-  Reddiv.prototype = new Box();
-
   var redBox = new Reddiv ('FF0000');
-
-
-
-  function Greendiv(color) {
-    Box.call (this, color);
-  }
-
-  Greendiv.prototype = new Box();
 
   var greenBox = new Greendiv ('00FF00');
 
-
-  //draw method
-  Box.prototype.draw = function() {
-    var newdiv = $('<div>');
-    $('body').append(newdiv);
-    newdiv.css('background-color', this.color);
-    newdiv.css('height', this.height);
-    newdiv.css('width', this.width);
-    var colorHold = this.color;
-    $(newdiv).click(function(){
-      alert(colorHold + ' has been clicked.');
-    });
-  };
-
-
   //command to draw box
-    blueBox.draw();
-    redBox.draw();
-    greenBox.draw();
+  blueBox.draw();
+  redBox.draw();
+  greenBox.draw();
+
 });
